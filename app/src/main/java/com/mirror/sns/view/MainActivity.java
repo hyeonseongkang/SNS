9c74744ccd58cff12b07cfa90ca6877e18a5818a
HyeonSeongKang <gt9697@naver.com>
2022-10-11 00:14:00 +0900

    add library

--- app/src/main/java/com/mirror/sns/view/MainActivity.java
+++ app/src/main/java/com/mirror/sns/view/MainActivity.java
@@ -1,8 +1,10 @@
-package com.mirror.sns;
+package com.mirror.sns.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.mirror.sns.R;
 
 public class MainActivity extends AppCompatActivity {
 
