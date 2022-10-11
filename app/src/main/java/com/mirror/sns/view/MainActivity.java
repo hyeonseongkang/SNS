4e6286dea9ef11f47f8bcbbbd6c05291ad8d0387
HyeonSeongKang <gt9697@naver.com>
2022-10-12 00:12:19 +0900

    bottomNavigationView fragment 연결

--- app/src/main/java/com/mirror/sns/view/MainActivity.java
+++ app/src/main/java/com/mirror/sns/view/MainActivity.java
@@ -1,16 +1,50 @@
 package com.mirror.sns.view;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import com.google.android.material.navigation.NavigationBarView;
 import com.mirror.sns.R;
+import com.mirror.sns.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = "MainActivity";
+
+    // view binding
+    private ActivityMainBinding mainBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        mainBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(mainBinding.getRoot());
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, new HomeFragment()).commit();
+
+        mainBinding.bottomNavigation.setOnItemSelectedListener(onItemSelectedListener);
     }
+
+    NavigationBarView.OnItemSelectedListener onItemSelectedListener = new NavigationBarView.OnItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch(item.getItemId()) {
+                case R.id.home:
+                    getSupportFragmentManager().beginTransaction().replace(R.id.container, new HomeFragment()).commit();
+                    return true;
+
+                case R.id.chat:
+                    getSupportFragmentManager().beginTransaction().replace(R.id.container, new SearchFragment()).commit();
+                    return true;
+
+                case R.id.mypage:
+                    getSupportFragmentManager().beginTransaction().replace(R.id.container, new MyPageFragment()).commit();
+                    return true;
+            }
+            return false;
+        }
+    };
 }
