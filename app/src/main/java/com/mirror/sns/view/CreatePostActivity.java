11349d32466074e09b899b67a2fbc358f146fd01
HyeonSeongKang <gt9697@naver.com>
2022-10-22 00:17:59 +0900

    post작성 화면

---
+++ app/src/main/java/com/mirror/sns/view/CreatePostActivity.java
@@ -0,0 +1,16 @@
+package com.mirror.sns.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.mirror.sns.R;
+
+public class CreatePostActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_post);
+    }
+}
