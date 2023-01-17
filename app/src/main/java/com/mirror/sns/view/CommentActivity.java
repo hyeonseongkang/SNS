c348b2be3c51f78245ded739bbf01e075024313a
HyeonSeongKang <gt9697@naver.com>
2023-01-18 07:02:11 +0900

    feat: CommentActivity 레이아웃

--- app/src/main/java/com/mirror/sns/view/CommentActivity.java
+++ app/src/main/java/com/mirror/sns/view/CommentActivity.java
@@ -2,9 +2,14 @@ package com.mirror.sns.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.mirror.sns.R;
+import com.mirror.sns.databinding.ActivityCommentBinding;
+import com.mirror.sns.databinding.ActivityDetailPostBinding;
 
 public class CommentActivity extends AppCompatActivity {
 
@@ -13,11 +18,29 @@ public class CommentActivity extends AppCompatActivity {
     // finish();
     //                overridePendingTransition(R.anim.none, R.anim.fadeout_left);
 
+    private ActivityCommentBinding commentBinding;
+
+    String userUid;
+    String itemKey;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_comment);
+        commentBinding = ActivityCommentBinding.inflate(getLayoutInflater());
+        setContentView(commentBinding.getRoot());
+
+        Intent intent = getIntent();
+        userUid = intent.getStringExtra("userUid");
+        itemKey = intent.getStringExtra("itemKey");
 
         overridePendingTransition(R.anim.fadein_left, R.anim.none);
+
+        commentBinding.backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+                overridePendingTransition(R.anim.none, R.anim.fadeout_left);
+            }
+        });
     }
 }
