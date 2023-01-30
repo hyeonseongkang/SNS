09a0b2562f8b027a4e81736b7d20a0c5bc18259e
HyeonSeongKang <gt9697@naver.com>
2023-01-30 20:29:08 +0900

    feat: 친구 추가 화면 layout

--- app/src/main/java/com/mirror/sns/view/AddFriendActivity.java
+++ app/src/main/java/com/mirror/sns/view/AddFriendActivity.java
@@ -3,6 +3,7 @@ package com.mirror.sns.view;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.mirror.sns.R;
 import com.mirror.sns.databinding.ActivityAddFriendBinding;
@@ -16,5 +17,14 @@ public class AddFriendActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         addFriendBinding = ActivityAddFriendBinding.inflate(getLayoutInflater());
         setContentView(addFriendBinding.getRoot());
+        overridePendingTransition(R.anim.fadein_left, R.anim.none);
+
+        addFriendBinding.backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+                overridePendingTransition(R.anim.none, R.anim.fadeout_left);
+            }
+        });
     }
 }
