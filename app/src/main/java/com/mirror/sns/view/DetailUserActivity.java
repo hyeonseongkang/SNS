7119cb6c5d838b71f47bccec89886d54550a237a
HyeonSeongKang <gt9697@naver.com>
2023-02-18 14:31:11 +0900

    feat: post이동

--- app/src/main/java/com/mirror/sns/view/DetailUserActivity.java
+++ app/src/main/java/com/mirror/sns/view/DetailUserActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.GridLayoutManager;
 
 import android.content.Intent;
 import android.net.Uri;
@@ -15,9 +16,14 @@ import android.widget.Toast;
 import com.bumptech.glide.Glide;
 import com.google.firebase.auth.FirebaseAuth;
 import com.mirror.sns.R;
+import com.mirror.sns.adapter.PostAdapter;
+import com.mirror.sns.classes.Post;
 import com.mirror.sns.classes.User;
 import com.mirror.sns.databinding.ActivityDetailUserBinding;
+import com.mirror.sns.viewmodel.PostViewModel;
 import com.mirror.sns.viewmodel.UserManagementViewModel;
+
+import java.util.List;
 
 public class DetailUserActivity extends AppCompatActivity {
 
@@ -25,6 +31,7 @@ public class DetailUserActivity extends AppCompatActivity {
 
     ActivityDetailUserBinding detailUserBinding;
 
+    private PostViewModel postViewModel;
     private UserManagementViewModel userManagementViewModel;
 
     String userUid;
@@ -32,13 +39,14 @@ public class DetailUserActivity extends AppCompatActivity {
     private User requestUser;
     private User responseUser;
 
+    private PostAdapter postAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         detailUserBinding = ActivityDetailUserBinding.inflate(getLayoutInflater());
         setContentView(detailUserBinding.getRoot());
         overridePendingTransition(R.anim.fadein_left, R.anim.none);
-
         Intent intent = getIntent();
         userUid = intent.getStringExtra("userUid");
 
@@ -61,7 +69,7 @@ public class DetailUserActivity extends AppCompatActivity {
                     detailUserBinding.followerCount.setText(followerSize);
                     detailUserBinding.followingCount.setText(followingSize);
 
-                    if (user.getPhotoUri().length() > 0 ) {
+                    if (user.getPhotoUri().length() > 0) {
                         Glide.with(DetailUserActivity.this)
                                 .load(Uri.parse(user.getPhotoUri()))
                                 .into(detailUserBinding.userPhoto);
@@ -73,6 +81,23 @@ public class DetailUserActivity extends AppCompatActivity {
 
             }
         });
+
+        postViewModel = new ViewModelProvider(this, new ViewModelProvider.AndroidViewModelFactory(getApplication())).get(PostViewModel.class);
+
+
+        detailUserBinding.postsRecyclerView.setLayoutManager(new GridLayoutManager(DetailUserActivity.this, 3));
+        detailUserBinding.postsRecyclerView.setHasFixedSize(true);
+        postAdapter = new PostAdapter();
+        detailUserBinding.postsRecyclerView.setAdapter(postAdapter);
+
+        postViewModel.getUserPosts(userUid);
+        postViewModel.getUserPosts().observe(this, new Observer<List<Post>>() {
+            @Override
+            public void onChanged(List<Post> posts) {
+                postAdapter.setPosts(posts);
+            }
+        });
+
 
         userManagementViewModel.getFollowRequest().observe(this, new Observer<Boolean>() {
             @Override
